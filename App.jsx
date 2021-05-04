b113ac38d2093ebc6532b6ef5bc5806a8a5f51a9
shirly <shirlyarcusin@gmail.com>
2021-05-04 11:54:24 +0300

    updates

--- App.jsx
+++ App.jsx
@@ -3,6 +3,8 @@ const { Route, Switch } = ReactRouterDOM
 
 import { AboutUs } from './pages/AboutUs.jsx'
 import { AppHeader } from './cmps/AppHeader.jsx'
+import {KeepApp} from './apps/Keep/KeepApp.jsx'
+import {MailApp} from './apps/Mail/MailApp.jsx'
 import {Home} from './pages/Home.jsx'
 
 
@@ -19,7 +21,10 @@ export function App() {
                     {/* <Route component={BookAdd} path="/add" /> */}
                     {/* <Route component={BookApp} path="/book" /> */}
                     <Route component={AboutUs} path="/about" />
+                    <Route component={KeepApp} path="/keep" />
+                    <Route component={MailApp} path="/mail" />
                     <Route component={Home} path="/" />
+
                 </Switch>
             </main>
 
